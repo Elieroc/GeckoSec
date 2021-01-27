6a57c8da4beb0334b27d5fe881db32d357ce9de0
Elieroc <elierocroc82@gmail.com>
2021-01-27 15:22:42 +0100

    Scan

---
+++ Pscanner.py
@@ -0,0 +1,80 @@
+ #!/usr/bin/python3
+
+import socket
+import threading
+
+target = '192.168.1.100'
+
+def port_scan(type, target, port=0):
+    if type == "1":
+        fast(target)
+    if type == "2":
+        medium(target)
+    if type == "3":
+        low(target)
+    if type == "4":
+        if port == 0:
+            return 0
+        else:
+            one(target, port)
+
+def one(target, port):
+    print("{*} Début du scan sur " + target + "\n")
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    timeout = sock.settimeout(0.2)
+    if sock.connect_ex((target,port)):
+        print("[-] Port " + str(port) + " fermé ! " + "\n")
+    else:
+        print("[+] Port " + str(port) + " ouvert ! " + "\n")
+    sock.close()
+
+    print("{*} Scan terminé !")
+
+def fast(target):
+    ports=[21, 22, 23, 53, 80, 443]
+
+    print("{*} Début du scan rapide sur " + target + "\n")
+
+    for port in ports :
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        timeout = sock.settimeout(0.2)
+        if sock.connect_ex((target,port)):
+            continue
+        else:
+            print("[+] Port " + str(port) + " ouvert ! " + "\n")
+        sock.close()
+
+    print("{*} Scan terminé !")
+
+def medium(target):
+    # Scan des 1024 premiers ports
+    print("{*} Début du scan à vitesse moyenne sur " + target + "\n")
+
+    for port in range(1,1025):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        timeout = sock.settimeout(0.1)
+        if sock.connect_ex((target,port)):
+            continue
+        else:
+            print("[+] Port " + str(port) + " ouvert ! " + "\n")
+        sock.close()
+
+    print("{*} Scan terminé !")
+
+def low(target):
+    # Scan de tous les ports (65536 ports)
+
+    print("{*} Début du scan complet sur " + target + "\n")
+
+    for port in range(1,65536):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        timeout = sock.settimeout(0.1)
+        if sock.connect_ex((target,port)):
+            continue
+        else:
+            print("[+] Port " + str(port) + " ouvert ! " + "\n")
+            sock.close()
+
+
+    print("{*} Scan terminé !")
--- geckosec.py
+++ geckosec.py
@@ -1,7 +1,56 @@
-#!usr/bin/python
+ #!/usr/bin/python3
 
 from Banner import banner
+from Pscanner import port_scan
+import time
+import os
 
-banner()
-print("\033[1;36m" + " Welcome to GeckoSec Framework\n")
-choice = input("\033[1;37m(\033[91m~\033[1;37m) > \033[1;35m")
+
+def main():
+    os.system('clear')
+    banner()
+    print("\033[1;36m" + " Welcome to GeckoSec Framework\n")
+    choice = inp()
+    if choice == "1":
+        scan()
+    else :
+        print("\033[91mBad option !")
+        time.sleep(1)
+        main()
+        os.system('clear')
+
+def inp():
+    inp = input("\033[1;37m(\033[91m~\033[1;37m) > \033[1;35m")
+    if inp == "menu":
+        main()
+    if inp == "exit":
+        exit(0)
+    if inp == "help":
+        help()
+    # Reset de la couleur
+    print('\033[1;37m ')
+    return inp
+
+def help():
+    print("\033[91mLa page d'aide est en construction !")
+    time.sleep(1)
+    main()
+
+def scan():
+    print("Bienvenue dans la partie Scan !\n")
+    print("1) Scan Ninja")
+    print("2) Scan des 1024 premiers ports")
+    print("3) Scan de tous les ports (pas discret)")
+    print("4) Scan d'un seul port\n")
+    type = str(inp())
+    print("Saisissez l'ip à target :")
+    target = str(inp())
+    if type == "4":
+        print("Saisissez le port à scanner :")
+        port = int(inp())
+        port_scan(type, target, port)
+    port_scan(type, target)
+    print("\n")
+    inp()
+
+main()
